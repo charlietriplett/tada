3e5f88d94717658f5be8d8ff31719c44a743657d
Charlie Triplett <charlietriplett@gmail.com>
2014-06-19 17:40:53 +0000

    Added spec for deleting todo items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Viewing todo items" do
+describe "Creating todo items" do
 
   let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries" )}
     
@@ -21,7 +21,28 @@ describe "Viewing todo items" do
     within("ul.todo_items") do
       expect(page).to have_content("Milk")
     end
-    
+  end
+  
+  it "displays an error with no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item")
+    end
+    expect(page).to have_content("Content can't be blank")
+  end
+ 
+  it "displays an error with content less than 2 char long" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item")
+    end
+    expect(page).to have_content("Content is too short")
   end
   
 
